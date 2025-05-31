6698ba2e89d3c12c32575f361e5b1639fffc2f0f
uddeshya gautam <uddeshyagautam@uddeshyas-MacBook-Pro.local>
2025-05-31 06:48:57 -0700

    add tailwind portfolio and redux toolkit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from './components/Navbar'
+import Skills from './components/Skills'
+import Hero from './components/Hero'
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Skills />
+    </>
+  )
+}
+export default App
