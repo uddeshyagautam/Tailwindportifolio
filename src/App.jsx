fb85fc2619e4e1752502330ea5eb5e58541e317a
uddeshya gautam <uddeshyagautam@uddeshyas-MacBook-Pro.local>
2025-05-31 21:25:11 -0700

    Add About section and Projects section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import Navbar from './components/Navbar'
 import Skills from './components/Skills'
 import Hero from './components/Hero'
+import About from './components/About'
+import Projects from './components/Projects'
 const App = () => {
   return (
     <>
       <Navbar />
       <Hero />
       <Skills />
+      <About />
+      <Projects />
     </>
   )
 }
